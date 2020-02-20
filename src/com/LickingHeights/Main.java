c9d72c44ef8e8058b1dfc28c64ee94ea37c66cfe
Brandon Peach <brandon.peach.2022@student.lhschools.org>
2020-02-20 12:06:58 -0500

    OddnessMethodCreated

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,8 +1,23 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+int number;
+
+Scanner keyboard = new Scanner(System.in);
+System.out.println("Enter a positive whole number");
+number = keyboard.nextInt();
+
+System.out.println("Number is odd: "+(number % 2 !=0));
+
+
+
+
+
+
     }
 }
