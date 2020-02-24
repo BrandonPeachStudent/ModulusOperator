44da5d3f7058d5a9de2f22189d1fe161504a8ef8
Brandon Peach <brandon.peach.2022@student.lhschools.org>
2020-02-24 12:19:21 -0500

    lastDigitMethodCreated

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -8,29 +8,26 @@ public class Main {
 
 int number;
 
-Scanner keyboard = new Scanner(System.in);
-System.out.println("Enter a positive whole number");
-number = keyboard.nextInt();
+            Scanner keyboard = new Scanner(System.in);
+            System.out.println("Enter a positive whole number");
+            number = keyboard.nextInt();
 
-System.out.println("Number is odd: "+(number % 2 !=0));
-
-
-
-
-        Scanner keep = new Scanner(System.in);
-        System.out.println("Enter another positive whole number");
-        number = keep.nextInt();
-
-        System.out.println(number+" contained is: "+(number % 8+5));
+            System.out.println("Number is odd: "+(number % 2 !=0));
 
 
 
+            Scanner keep = new Scanner(System.in);
+            System.out.println("Enter another positive whole number");
+            number = keep.nextInt();
+
+            System.out.println(number+" contained is: "+(number % 8+5));
 
 
 
+            Scanner last = new Scanner(System.in);
+            System.out.println("Enter a positive number that is 4 digits long");
+            number = last.nextInt();
 
-
-
-
+            System.out.println("The last digit of "+number+" is: "+number%10);
     }
 }
